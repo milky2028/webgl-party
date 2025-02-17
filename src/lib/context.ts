3aa0fd433a422456f9d73590679647fc5e7f8d0e
Tyler Gross <tylergross28@gmail.com>
2025-02-17 09:27:41 -0500

    make canvas a little bigger

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,6 +1,6 @@
 export const canvas = document.createElement('canvas');
 canvas.id = 'rendering-canvas';
 canvas.width = window.innerWidth;
-canvas.height = 500;
+canvas.height = 600;
 
 export const gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
